3d5b275be5ca4a1feec71e2058e3dd06eae97619
Reuben <2824016820@qq.com>
2022-07-01 23:48:45 +0800

    mipmap

--- TinyMetalEngine/TinyMetalEngine/Runtime/TextureController.swift
+++ TinyMetalEngine/TinyMetalEngine/Runtime/TextureController.swift
@@ -15,7 +15,10 @@ enum TextureController {
     /// - Returns: 贴图
     static func loadTexture(filename: String) throws -> MTLTexture? {
         let textureLoader = MTKTextureLoader(device: Renderer.device)
-        let textureLoaderOptions: [MTKTextureLoader.Option: Any] = [.origin: MTKTextureLoader.Origin.bottomLeft, .SRGB: false]
+        //贴图加载机制（零点位置，伽马矫正，Mipmap）
+        let textureLoaderOptions: [MTKTextureLoader.Option: Any] = [.origin: MTKTextureLoader.Origin.bottomLeft,
+                                                                    .SRGB: false,
+                                                                    .generateMipmaps: NSNumber(value: true)]
         let fileExtension = URL(fileURLWithPath: filename).pathExtension.isEmpty ? "png" : nil
         
         guard let url = Bundle.main.url(forResource: filename, withExtension: fileExtension) else{
--- TinyMetalEngine/TinyMetalEngine/ShaderLibrary/Shaders/Fragment.metal
+++ TinyMetalEngine/TinyMetalEngine/ShaderLibrary/Shaders/Fragment.metal
@@ -13,7 +13,7 @@ using namespace metal;
 
 fragment float4 fragment_main(constant Params &params [[buffer(12)]], VertexOut in [[stage_in]], texture2d<float> baseColorTexture [[texture(BaseColor)]])
 {
-    constexpr sampler textureSampler(filter::linear, address::repeat);   //采样器，采样方式为线性滤波+重复
+    constexpr sampler textureSampler(filter::linear, address::repeat, mip_filter::linear);   //采样器
     float3 baseColor = baseColorTexture.sample(textureSampler, in.uv * params.tiling).rgb;
     return float4(baseColor, 1);
 }
