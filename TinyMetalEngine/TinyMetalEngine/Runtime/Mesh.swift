1909c109e8f57a867fa4a989e682971974388f06
Reuben-Sun <2824016820@qq.com>
2022-07-01 19:00:23 +0800

    mesh 和 submesh

--- TinyMetalEngine/TinyMetalEngine/Runtime/Mesh.swift
+++ TinyMetalEngine/TinyMetalEngine/Runtime/Mesh.swift
@@ -5,4 +5,25 @@
 //  Created by bytedance on 2022/7/1.
 //
 
-import Foundation
+import MetalKit
+
+// swiftlint:disable force_unwrapping
+// swiftlint:disable force_cast
+
+struct Mesh {
+  let vertexBuffers: [MTLBuffer]
+  let submeshes: [Submesh]
+}
+
+extension Mesh {
+  init(mdlMesh: MDLMesh, mtkMesh: MTKMesh) {
+    var vertexBuffers: [MTLBuffer] = []
+    for mtkMeshBuffer in mtkMesh.vertexBuffers {
+      vertexBuffers.append(mtkMeshBuffer.buffer)
+    }
+    self.vertexBuffers = vertexBuffers
+    submeshes = zip(mdlMesh.submeshes!, mtkMesh.submeshes).map { mesh in
+      Submesh(mdlSubmesh: mesh.0 as! MDLSubmesh, mtkSubmesh: mesh.1)
+    }
+  }
+}
--- TinyMetalEngine/TinyMetalEngine/Runtime/Submesh.swift
+++ TinyMetalEngine/TinyMetalEngine/Runtime/Submesh.swift
@@ -5,4 +5,20 @@
 //  Created by bytedance on 2022/7/1.
 //
 
-import Foundation
+import MetalKit
+
+struct Submesh {
+  let indexCount: Int
+  let indexType: MTLIndexType
+  let indexBuffer: MTLBuffer
+  let indexBufferOffset: Int
+}
+
+extension Submesh {
+  init(mdlSubmesh: MDLSubmesh, mtkSubmesh: MTKSubmesh) {
+    indexCount = mtkSubmesh.indexCount
+    indexType = mtkSubmesh.indexType
+    indexBuffer = mtkSubmesh.indexBuffer.buffer
+    indexBufferOffset = mtkSubmesh.indexBuffer.offset
+  }
+}
