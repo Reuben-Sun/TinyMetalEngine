defdbd02cc9c78221788fb4946d546698a2203a2
Reuben-Sun <2824016820@qq.com>
2022-07-01 13:58:00 +0800

    项目结构初始化

---
+++ TinyMetalEngine/TinyMetalEngine/UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinyMetalEngine
+//
+//  Created by bytedance on 2022/7/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinyMetalEngine/TinyMetalEngine/UI/TinyMetalEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyMetalEngineApp.swift
+//  TinyMetalEngine
+//
+//  Created by bytedance on 2022/7/1.
+//
+
+import SwiftUI
+
+@main
+struct TinyMetalEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
